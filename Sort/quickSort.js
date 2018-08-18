5208831aa649ead5bc05107c2cfe24ad3a5837e4
NeuTrix <neutrix@brandgeneering.com>
2018-08-17 23:35:01 -0700

    Complete quicksort

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -1,5 +1,23 @@
-const quickSort = function(arr) {
+const quickSort = function(arr, counter =0) {
+  if (arr.length === 0) {
+    return [];
+  }
+  // return arr
+  let lesser = [];
+  let greater = [];
+  let pivot = arr[0];
+  // let counter = counter;
 
+  for (let i = 1; i < arr.length; i += 1) {
+    if (arr[i] > pivot) {
+      greater.push(arr[i])
+    } else {
+      lesser.push(arr[i])
+    }
+  }
+
+  // stitch the arrays and the pivot
+  return quickSort(lesser).concat(pivot, quickSort(greater))
 }
 
 module.exports = quickSort
--- Sort/test/quickSort.test.js
+++ Sort/test/quickSort.test.js
@@ -1,12 +1,18 @@
 let chai = require('chai');
 let expect = chai.expect;
-
 let quick = require('../quickSort')
 
 describe('The quickSort function', () => {
+  // let arr = []
+  // for (let i =0; i < 100; i += 1) {
+  //   let num = Math.floor((Math.random()*100)+1)
+  //   arr.push(num)
+  // }
+  // console.log('test ==>',arr)
   let arr = [14, 5, 19, 22, 4, 8, 11, 2, 30, 12]
   let ans = [2, 4, 5, 8, 11, 12, 14, 19, 22, 30]
 
+  console.log(quick(arr))
   it('... sorts an array', () => {
     expect(quick(arr)).to.eql(ans)
   });
