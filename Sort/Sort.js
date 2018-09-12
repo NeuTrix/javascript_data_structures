91ac3f17e035f265b49ee259672a0beec43d87d4
NeuTrix <neutrix@brandgeneering.com>
2018-09-12 07:10:48 -0700

    set basic selection sort test

--- Sort/Sort.js
+++ Sort/Sort.js
@@ -1,13 +1,22 @@
 // various sorting algortihmm in js
 class Sort {
 
+  // static print(func) {
+  //   console.time('test')
+  //   console.log(`xxxx  ${func}  xxxx`)
+  //   return func
+  // }
+
   static bubble(arr = []) {
     if (!arr.length > 1) {
       throw new Error('Array arg is empty or null.')
     }
     let counter = 0;
-      console.log(`\n ${'*'.repeat(30)}\n\n`)
-      console.log(` starting Array:  [ ${arr} ] \n`)
+    // print function
+    console.log(`\n ${'*'.repeat(15)} Bubble Sort ${'*'.repeat(15)}\n\n`)
+    console.time(`\t Bubble Sort Time`);
+    console.log(` starting Array:  [ ${arr} ] \n`)
+
     for (let outer = arr.length; outer >= 2; outer--) {
       for (let inner = 0; inner <= outer - 2; inner++) {
         if (arr[inner] > arr[inner + 1]) {
@@ -19,7 +28,31 @@ class Sort {
       counter++;
       console.log(` iteration: ${counter} ==> [ ${arr} ] \n`)
     }
-      console.log(`\n ${'*'.repeat(30)}\n\n`)
+      console.timeEnd(`\t Bubble Sort Time`);
+      console.log(`\n ${'*'.repeat(40)}\n\n`);
+    return arr;
+  }
+
+  static selection(arr = []) {
+    if (!arr.length > 1) {
+      throw new Error('Array arg is empty or null.')
+    }
+
+    let counter = 0;
+    let minimum = arr[0]
+      console.log(`\n ${'*'.repeat(15)} Selection Sort ${'*'.repeat(15)}\n\n`)
+      console.time(`\t Selection Sort Time`);
+      console.log(` starting Array:  [ ${arr} ] \n`)
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (arr)
+
+
+      counter++;
+      console.log(` iteration: ${counter} ==> [ ${arr} ] \n`)
+    }
+      console.timeEnd(`\t Selection Sort Time`);
+      console.log(`\n ${'*'.repeat(40)}\n\n`);
     return arr;
   }
 
--- Sort/test/Sort.test.js
+++ Sort/test/Sort.test.js
@@ -6,11 +6,6 @@ describe('The Sorting class', () => {
   
   const unsortedArr = [14, 5, 19, 22, 4, 8, 11, 2, 30, 12];
   const sortedArr = [2, 4, 5, 8, 11, 12, 14, 19, 22, 30];
-  
-  it('... creates a new Sort instance', () => {
-    const sort = new Sort(unsortedArr);
-    expect(sort instanceof Sort).to.eql(true);
-  });
 
   describe('The Bubble Sort algorithm', () => {
 
@@ -18,35 +13,47 @@ describe('The Sorting class', () => {
       expect(Sort.bubble).to.be.a('function');
     });
 
-    xit('... returns an error for a null array', () => {
-      // how to keep program from breaking on this test
-      let arr = [];
-      expect(Sort.bubble(arr)).to.throw(Error);
-    });
-
-    xit('... returns an error for single element array', () => {
-      let arr = [69]
-      expect(Sort.bubble(arr)).to.throw(Error);
-    });
-
     it('... sorts a very small arr of n = 2', () => {
       let arr = [69, 33]
       expect(Sort.bubble(arr)).to.eql([33, 69]);
     });
 
-    xit('... sorts an array with default args to Sort()', () => {
-      expect(Sort.bubble()).to.eql(sortedArr);
+    it('... sorts with args passed to #bubble of Sort', () => {
+      expect(Sort.bubble(unsortedArr)).to.eql(sortedArr);
     });
 
-    xit('... sorts with args passed to #bubble of Sort', () => {
-      const unsorted_B = [ 4, 3, 6, 7, 2, 1, 0, 5 ]
-      const sorted_B   = [ 0, 1, 2, 3, 4, 5, 6, 7 ]
-      expect(Sort.bubble(unsorted_B)).to.eql(sorted_B);
-    });
+    /* xit('... can print a function', () => {
+      Sort.print(Sort.bubble(unsortedArr))
+    }); */
+
+    /* xit('... returns an error for a null array', () => {
+      // how to keep program from breaking on this test
+      let arr = [];
+      expect(Sort.bubble(arr)).to.throw(Error);
+    }); */
+
+    /* xit('... returns an error for single element array', () => {
+      let arr = [69]
+      expect(Sort.bubble(arr)).to.throw(Error);
+    }); */
 
   });
 
+  describe('The Selection Sort algorithm', () => {
 
+    it('... is a viable function', () => {
+      expect(Sort.selection).to.be.a('function');
+    });
 
+    it('... sorts a very small arr of n = 2', () => {
+      let arr = [69, 33]
+      expect(Sort.selection(arr)).to.eql([33, 69]);
+    });
+
+    xit('... sorts with args passed to #selection of Sort', () => {
+      expect(Sort.selection(unsortedArr)).to.eql(sortedArr);
+    });
+
+  });
 
 });
