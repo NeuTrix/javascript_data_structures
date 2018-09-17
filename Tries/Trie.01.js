e540b31d627283a2a1fd2168b1d6b49b5c0fbc0a
NeuTrix <neutrix@brandgeneering.com>
2018-09-16 18:36:14 -0700

    tweaks

--- Tries/Trie.01.js
+++ Tries/Trie.01.js
@@ -1,51 +1,53 @@
+let chalk = require('chalk')
+
 class Node {
-  constructor(value = null) {
-    this.value = value;
-    this.parent = {};
-    this.children = new Map();
-    this.isWordEnd = false;
+  constructor (value = null) {
+    this.value = value
+    this.parent = {}
+    this.children = new Map()
+    this.isWordEnd = false
   }
 }
 
 class Trie {
-  constructor() {
-    this.root = new Node('root');
+  constructor () {
+    this.root = new Node('root')
   }
 
-  insert(str = null, node = this.root, memo = []) {
+  insert (str = null, node = this.root, memo = []) {
+    console.log('====> ', node.value)
+    // this.print(node)
+
     if (!str) { // validation
       this.report(memo.join(''))
       return memo.join('')
-    } 
-    
-    const kids = node.children; // node kids map
-    const char = str[0]; // first character of remaining string
-    memo.push(char); // populate memo memo
+    }
+
+    const kids = node.children // node kids map
+    const char = str[0] // first character of remaining string
+    memo.push(char) // populate memo memo
 
     // add to map if not currently included
-    if (!kids.has(char)) { 
-      kids.set(char, new Node(char));
-    } 
+    if (!kids.has(char)) {
+      kids.set(char, new Node(char))
+    }
 
     // settings for final character
     if (str.length === 1) {
       let charNode = kids.get(char)
-      charNode.isWordEnd = true;
-      console.log(`\nThe node for character "${char}" is set to "${charNode.isWordEnd}"`)
+      charNode.isWordEnd = true
+      console.log(`\n[isWord] prop for "${char}" is set to "${charNode.isWordEnd}"`)
     }
-    ads = 7
-    
-
     // reduce memo and recurse
     return this.insert(str.slice(1), kids.get(char), memo)
   }
 
   // ========= HELPER METHODS =========
-  report(memo) {
+  report (memo) {
     console.log(`\n${'='.repeat(40)} \n ==> The word "${memo}" has been added!\n${'='.repeat(40)} \n `)
     console.log(this)
     console.log(`\n${'='.repeat(40)}`)
   }
 }
 
-module.exports = {Trie, Node}
+module.exports = { Trie, Node }
--- Tries/test/Trie.01.test.js
+++ Tries/test/Trie.01.test.js
@@ -1,103 +1,96 @@
-const { Trie, Node } = require('../Trie.01');
-const chai = require('chai');
-const expect = chai.expect;
+const { Trie, Node } = require('../Trie.01')
+const chai = require('chai')
+const expect = chai.expect
 
 describe('The Node Class', () => {
-  const node = new Node();
+  const node = new Node()
 
   it('creates a new Node instance', () => {
-    expect(node instanceof Node).to.eql(true);
-  });
+    expect(node instanceof Node).to.eql(true)
+  })
 
   it('has (4) properties', () => {
     let keys = Object.keys(node)
-    expect(keys.length).to.eql(4);
-  });
+    expect(keys.length).to.eql(4)
+  })
 
   it('has a default value of `null`', () => {
-    expect(node.value).to.eql(null);
-  });
+    expect(node.value).to.eql(null)
+  })
 
   it('has a value property', () => {
     expect(node).to.have.property('value')
-  });
+  })
 
   it('has a parent object property', () => {
     expect(node).to.have.property('parent')
-      .to.be.an('object');
-  });
-  
+      .to.be.an('object')
+  })
+
   it('has a children map property', () => {
     expect(node).to.have.property('children')
-      .to.be.a('map');
-  });
-  
+      .to.be.a('map')
+  })
+
   it('has a isWordEnd boolean property', () => {
     expect(node).to.have.property('isWordEnd')
       .to.be.a('boolean')
-  });
-});
+  })
+})
 
 describe.only('The Trie Class', () => {
   let trie
 
   beforeEach(() => {
     trie = new Trie()
-  });
+  })
+
 
   it('creates a new Trie instance', () => {
-    expect(trie instanceof Trie).to.eql(true);
-  });
+    expect(trie instanceof Trie).to.eql(true)
+  })
 
   it('has a root property', () => {
     expect(trie).has.property('root')
-  });
+  })
 
   describe('=> the #insert method', () => {
-
     it('Trie had the #insert method', () => {
       expect(trie.insert).to.be.a('function')
-    });
-    
+    })
+
     it('returns an error if no string is passed', () => {
-      expect(trie.insert()).to.eql('');
-    });
+      expect(trie.insert()).to.eql('')
+    })
 
     it('can insert a single character', () => {
-      expect(trie.insert('F')).to.eql('F');
-    });
+      expect(trie.insert('F')).to.eql('F')
+    })
 
-    it('can insert a word', () => {
-      expect(trie.insert('monkey')).to.eql('monkey');
-    });
+    it.only('can insert a word', () => {
+      trie.print()
+      expect(trie.insert('monkey')).to.eql('monkey')
+    })
 
     it.only('returns name of inserted sub-string', () => {
       trie.insert('monkey')
-      let substring = trie.insert('money');
+      let substring = trie.insert('money')
       expect(substring).equal('money')
-    });
+    })
 
     it('marks the end of a word', () => {
-      const kids = trie.root.children;
-      let test = trie.insert('e');
+      const kids = trie.root.children
+      let test = trie.insert('e')
       // console.log(kids.get('e'))
-      expect(kids.get('e').isWordEnd).to.eql(true);
-    });
-    
-  
-
-  });
+      expect(kids.get('e').isWordEnd).to.eql(true)
+    })
+  })
 
   describe('=> the #find method', () => {
-    
-  });
+
+  })
 
   describe('=> the #delete method', () => {
-    
-  });
 
-
-
-});
-
-
+  })
+})
